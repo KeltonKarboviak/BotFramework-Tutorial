b567ae22b85309b82ff03734efd483bb2e8f265b
Kelton Karboviak <kelton.karboviak@digikey.com>
2017-08-10 14:28:11 -0500

    Added a simple count State variable

--- DigiBot-Tutorial/Controllers/MessagesController.cs
+++ DigiBot-Tutorial/Controllers/MessagesController.cs
@@ -24,34 +24,37 @@ namespace DigiBot_Tutorial
             {
                 HandleSystemMessage(activity);
             }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private Activity HandleSystemMessage(Activity activity)
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
+            switch (activity.Type)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
+                case ActivityTypes.DeleteUserData:
+                    // Implement user deletion here
+                    // If we handle user deletion, return a real message
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    // Handle conversation state changes, like members being added and removed
+                    // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                    // Not available in all channels
+                    break;
+
+                case ActivityTypes.ContactRelationUpdate:
+                    // Handle add/remove from contact lists
+                    // Activity.From + Activity.Action represent what happened
+                    break;
+
+                case ActivityTypes.Typing:
+                    // Handle knowing that the user is typing
+                    break;
+
+                case ActivityTypes.Ping:
+                    // Represents an attempt to determine whether a bot's endpoint is accessible.
+                    break;
             }
 
             return null;
--- DigiBot-Tutorial/Dialogs/RootDialog.cs
+++ DigiBot-Tutorial/Dialogs/RootDialog.cs
@@ -8,6 +8,8 @@ namespace DigiBot_Tutorial.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+        protected int count = 1;
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -15,17 +17,40 @@ namespace DigiBot_Tutorial.Dialogs
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> activity)
         {
-            var activity = await result as Activity;
+            var message = await activity;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    this.ResumeAfterResetDialog,
+                    "Are you sure you want to reset the count?",
+                    "Sorry, I didn't get that.",
+                    promptStyle: PromptStyle.None);
+            }
+            else
+            {
+                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                context.Wait(this.MessageReceivedAsync);
+            }
+        }
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+        private async Task ResumeAfterResetDialog(IDialogContext context, IAwaitable<bool> result)
+        {
+            var confirm = await result;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
 
-            context.Wait(MessageReceivedAsync);
+            context.Wait(this.MessageReceivedAsync);
         }
     }
 }
